7fc71dd8e835d72a2c61414a35cd19ac39cc41d5
Serhii Voichyk <svoychik@gmail.com>
2021-04-10 23:31:17 +0300

    Testing code deploy

--- Deploy.ps1
+++ Deploy.ps1
@@ -1,3 +1,4 @@
 
 aws cloudformation deploy --stack-name sevolabs-web --template-file .\cloudformation.template.yml
-aws s3 sync ./web/dist s3://sevolabs.com
+npm run build
+aws s3 sync ./dist s3://sevolabs.com
