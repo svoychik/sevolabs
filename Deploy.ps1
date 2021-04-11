2b85e9af3eec7550b24b01e0f257741ed577a5f3
Serhii Voichyk <svoychik@gmail.com>
2021-04-11 21:10:04 +0300

    moved to sh scripts

--- Deploy.ps1
+++
@@ -1,4 +0,0 @@
-
-aws cloudformation deploy --stack-name sevolabs-web --template-file .\cloudformation.template.yml
-npm run build
-aws s3 sync ./dist s3://sevolabs.com
---
+++ Deploy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+aws cloudformation deploy --stack-name sevolabs-web --template-file ./cloudformation.template.yml
+npm run build
+aws s3 sync ./dist s3://sevolabs.com
